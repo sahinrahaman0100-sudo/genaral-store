1ad95024d281adaab8c67afd83a13796237e8be6
Sahin Rahaman <sahinrahaman0100@gmail.com>
2026-04-14 11:18:33 +0530

    Delete krishna-store directory

--- krishna-store/src/hooks/useTranslation.ts
+++
@@ -1,8 +0,0 @@
-import { useLanguage } from '@/store';
-import { translations, type TranslationKey } from '@/i18n/translations';
-
-export function useTranslation() {
-  const language = useLanguage();
-  const t = (key: TranslationKey): string => translations[language][key];
-  return { t, language };
-}
